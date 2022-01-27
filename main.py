cf77ac8462a37e3e7182c64049297f92d3578dfa
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-01-28 01:18:39 +0530

    Done

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
 from filesharer import FileSharer
-import cv2
+import webbrowser
 
 
 Builder.load_file('frontend.kv')
@@ -12,23 +12,50 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
+        """Starts camera and changes Button text"""
+        self.ids.camera.opacity = 1
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        """Stops camera and changes Button text"""
+        self.ids.camera.opacity = 0
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
 
     def capture(self):
+        """Create a filename with the current time and captures and saves a photo image under that filename"""
         current_time = time.strftime('%Y%m%d-%H%M%S')
-        filepath = f"files/{current_time}.png"
-        self.ids.camera.export_to_png(filepath)
+        self.filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(self.filepath)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = self.filepath
 
 
 class ImageScreen(Screen):
-    pass
+    link_message = "Create a Link First"
+    def create_link(self):
+        """Accesses the photo filepath, uploads it to the web and inserts the link in the label widget"""
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        filesharer = FileSharer(filepath = file_path)
+        self.url = filesharer.share()
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        """Copy link to the clipboard available for pasting"""
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        """Open link with default browser"""
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
 
 
 class RootWidget(ScreenManager):
